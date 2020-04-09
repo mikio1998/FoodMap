b008c1fbc3acf7f9f410177ec9af74693a91e496
Mikio <21nakatam@gmail.com>
2020-04-09 16:34:43 +0900

    onboarding

---
+++ Snacks/Controllers/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  Snacks
+//
+//  Created by Mikio Nakata on 4/9/20.
+//  Copyright © 2020 Mikio Nakata. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    
+}
---
+++ Snacks/Model/StoneLocation.swift
@@ -0,0 +1,9 @@
+//
+//  StoneLocation.swift
+//  Snacks
+//
+//  Created by Mikio Nakata on 4/9/20.
+//  Copyright © 2020 Mikio Nakata. All rights reserved.
+//
+
+import Foundation
